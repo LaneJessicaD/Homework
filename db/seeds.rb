f56a8bcabbd5a0a203a9f12d6c5ff768b7360cb4
vagrant <vagrant@kellogg-dev-box>
2014-04-21 20:40:43 +0000

    Building Tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 
-book = Book(id: integer, title: string, author: string)
-
 Book.create(:title => "A Brief History of Time", :author => "Stephen Hawking")
 Book.create(:title => "Alice in Wonderland", :author => "Lewis Carroll")
 Book.create(:title => "The Lord Of The Rings", :author => "J.R.R. Tolkien")
