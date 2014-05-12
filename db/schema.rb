c82ba7f44d0550f61bc043ca85cf1121e6737076
vagrant <vagrant@kellogg-dev-box>
2014-05-12 19:49:10 +0000

    Created 2 Models

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421202825) do
+ActiveRecord::Schema.define(version: 20140512191837) do
 
   create_table "books", force: true do |t|
     t.string "title"
     t.string "author"
+  end
+
+  create_table "bookstores", force: true do |t|
+    t.string  "title"
+    t.string  "author"
+    t.integer "price"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer "title"
+    t.integer "starrating"
+    t.text    "review"
   end
 
 end
